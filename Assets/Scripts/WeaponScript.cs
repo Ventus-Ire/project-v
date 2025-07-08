e207c4c663847367e5d3e8edfd9a1bcdd7a96e53
Amit Nemani <wasabi.3122@gmail.com>
2025-07-07 20:40:35 -0400

    added first script to cause a button to trigger text based on an input action

--- Assets/Scripts/WeaponScript.cs
+++ Assets/Scripts/WeaponScript.cs
@@ -3,5 +3,39 @@ using UnityEngine.InputSystem;
 
 public class WeaponScript : MonoBehaviour
 {
-    //private InputActionReference
+    [SerializeField] InputActionReference shootAction;
+
+    private void Awake()
+    {
+        if(shootAction != null )
+        {
+            shootAction.action.Enable();
+            shootAction.action.performed += Shoot;
+            InputSystem.onDeviceChange += OnDeviceChange;
+        }
+    }
+    private void OnDestory()
+    {
+        shootAction.action.Disable();
+        shootAction.action.performed -= Shoot;
+        InputSystem.onDeviceChange -= OnDeviceChange;
+    }
+
+    private void Shoot(InputAction.CallbackContext context)
+    {
+        Debug.Log("I ACTUALLY SHOT SOMETHING");
+    }
+
+    private void OnDeviceChange(InputDevice device, InputDeviceChange change)
+    {
+        switch(change)
+        {
+            case InputDeviceChange.Disconnected:
+                shootAction.action.Disable();
+                break;
+            case InputDeviceChange.Reconnected:
+                shootAction.action.Enable();
+                break;
+        }
+    }
 }
